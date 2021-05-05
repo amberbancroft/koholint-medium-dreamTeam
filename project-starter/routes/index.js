22e0b61fde66016a9e9480b4bb5e5510f1e881f5
Rebecca <rebeccawchen@gmail.com>
2021-05-05 10:23:32 -0500

    root styling, imported story data

--- project-starter/routes/index.js
+++ project-starter/routes/index.js
@@ -1,15 +1,26 @@
 //Imports
 const express = require("express");
+const {asyncHandler} = require("./utils");
+const db = require("../db/models");
 const router = express.Router();
 
+const getTenStories = async () => {
+  const allStories = await db.Story.findAll();
+  return allStories.slice(0,10);
+}
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  console.log(res.locals);
+router.get("/", asyncHandler(async (req, res, next) => {
+  const storiesToRender = await getTenStories();
+  const firstSixStories = storiesToRender.slice(0,6);
+  const lastFourStories = storiesToRender.slice(6);
   res.render("index", {
     title: "Home",
     authenticated: res.locals.authenticated,
+    topStories: firstSixStories,
+    otherStories: lastFourStories
   });
-});
+}));
 
 //Exports
 module.exports = router;
