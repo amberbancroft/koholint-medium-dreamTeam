b1b98d62059552379b8b8f651d7b43eeaa8570eb
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-05-05 17:39:07 -0400

    Created Edit Story Route

--- project-starter/routes/index.js
+++ project-starter/routes/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { csrfProtection, asyncHandler} = require("./utils");
 const db = require("../db/models");
+const { validationResult, check } = require("express-validator");
 const router = express.Router();
 
 const getTenStories = async () => {
@@ -35,9 +36,69 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async(req, res) => {
   })
 }));
 
+const storyValidator =[
+  check('title')
+      .exists({checkFalsy: true})
+      .withMessage("Title cannot be empty")
+      .custom((value)=>{
+          return db.Story.findOne({ where: {title:value}}).then((title)=>{
+              if (title)
+                  return Promise.reject("This title already exists");
+          })
+      }),
+  check('content')
+      .exists({checkFalsy: true})
+      .withMessage("Story cannot be empty")
+      .custom((value)=>{
+          return db.Story.findOne({ where: {content:value}}).then((story)=>{
+              if (story)
+                  return Promise.reject("This story already exists");
+          })
+      })
+]
+
 router.get('/:id(\\d+)/edit', csrfProtection, asyncHandler(async(req, res) => {
-  
+  const id = req.params.id
+  const story = await db.Story.findOne({
+    where: {id}
+  })
+  res.render('edit-story', {
+    csrfToken: req.csrfToken(),
+    story,
+    title: 'Edit Story',
+  })
 }))
+
+router.post('/:id(\\d+)/edit', csrfProtection, asyncHandler(async(req, res) => {
+  const id = req.params.id
+  const storyToUpdate = await db.Story.findOne({
+    where: {id}
+  });
+  
+  const {
+    title,
+    content,
+  } = req.body;
+
+  const story = {
+    title,
+    content,
+  }
+
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    await storyToUpdate.update(story);
+    res.redirect('/stories');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg)
+    res.render('edit-story', {
+      csrfToken: req.csrfToken(),
+      errors,
+      title: 'Edit Story',
+    })
+  }
+}));
 
 //Exports
 module.exports = router;
