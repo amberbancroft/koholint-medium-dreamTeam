78e5e5b6e058a718b80e5c2f82b542e27fbb6aaa
Justin <just2rules@gmail.com>
2021-05-06 09:09:01 -0700

    allowed nullable on imgUrl models

--- project-starter/db/models/story.js
+++ project-starter/db/models/story.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Story = sequelize.define('Story', {
     title: {type: DataTypes.STRING(30), allowNull: false },
-    imgUrl: {type: DataTypes.STRING(255), allowNull: false, unique: true},
+    imgUrl: {type: DataTypes.STRING(255), allowNull: true, unique: true},
     content: {type: DataTypes.TEXT, allowNull: false, unique: true},
     userId: {type: DataTypes.INTEGER, allowNull: false, references: {model: 'Users'}, unique: true}
   }, {});
