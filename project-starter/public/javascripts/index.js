216e6dc12b4be6de09bb492479be60d68b8f933e
Rebecca <rebeccawchen@gmail.com>
2021-05-06 19:14:16 -0500

    comment box

--- project-starter/public/javascripts/index.js
+++ project-starter/public/javascripts/index.js
@@ -1,3 +1,7 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+//If comment is clicked, reveal comment box
+const commentBox = document.querySelector("#comment-box");
+document.querySelector(".story-comment").addEventListener("click", e => {
+    commentBox.classList.toggle("hidden");
+});
+
+// document.querySelector("#comment-box > ")
