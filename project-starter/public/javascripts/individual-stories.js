cb1a74c2f9cffea57e059dadff2411ec15dbf954
Rebecca <rebeccawchen@gmail.com>
2021-05-06 22:57:52 -0500

    icon styling, more comment box styling

--- project-starter/public/javascripts/individual-stories.js
+++ project-starter/public/javascripts/individual-stories.js
@@ -1,23 +1,37 @@
 //If comment is clicked, reveal comment box
 console.log("hello world");
 const commentBox = document.querySelector("#comment-box");
-document.querySelector(".story-comment").addEventListener("click", e => {
+const commentButton = document.querySelector(".story-comment")
+commentButton.addEventListener("click", e => {
     commentBox.classList.toggle("hidden");
+    commentButton.classList.toggle("fa-lg");
 });
-// document.querySelector("#comment-box > ")
+document.querySelector(".comment-exit > i").addEventListener("click", e => {
+    commentBox.classList.toggle("hidden");
+    commentButton.classList.toggle("fa-lg");
+});
+
+document.querySelector(".comment-form").addEventListener("submit", e => {
+    e.preventDefault();
+})
 
 
 
 
-// const like = document.querySelector('.story-likes');
-// like.addEventListener('click', () => {
-//     fetch('/:id(\\d+)', {
-//         method: "PATCH",
-//     })
-// }).then(function(res) {
-//     if (!res.ok) {
-//         throw Error(res.statusText); 
-//     }
-//     return res.json();
-// })
-// .then()
+//Becky changed this code to grab the heart icon instead of the button
+const like = document.querySelector('#like-button');
+like.addEventListener('click', (e) => {
+    e.target.classList.add("fa-lg");
+    setTimeout(()=> {
+    e.target.classList.remove("fa-lg");
+    }, 100)
+    // fetch('/:id(\\d+)', {
+    //     method: "PATCH",
+    // }).then(function(res) {
+    //     if (!res.ok) {
+    //         throw Error(res.statusText); 
+    //     }
+    //     return res.json();
+    // })
+    // .then()
+})
