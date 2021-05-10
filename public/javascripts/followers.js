b8cfc275cd198baca0c0b6cc960743fdcc5b5753
Justin <just2rules@gmail.com>
2021-05-10 00:09:57 -0700

    followers buttons

---
+++ public/javascripts/followers.js
@@ -0,0 +1,31 @@
+const followButton = document.querySelectorAll('.green-btn-follow');
+
+followButton.forEach((element)=>
+element.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.innerText === "Follow") {
+            fetch(`/users/${element.dataset.value}/followers`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({followId: e.target.dataset.follow}),
+            }).then(function(res) {
+                element.innerText = "Following"
+            }).catch((error) => {
+                console.error(error)
+            })
+        } else {
+            fetch(`/users/${element.dataset.value}/followers`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({followId: e.target.dataset.follow}),
+            }).then(function(res) {
+                element.innerText = "Follow"
+            }).catch((error) => {
+                console.error(error)
+            })
+        }
+    }))
