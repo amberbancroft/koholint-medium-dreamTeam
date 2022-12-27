573ec3e99462e77740424de89720ec10aabc0adb
Justin Wong <just2rules@gmail.com>
2022-12-26 17:47:16 -0800

    fixed merge conflicts

--- psql-setup-script.js
+++ psql-setup-script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // require("dotenv").config();
-=======
-require("dotenv").config();
->>>>>>> ebd3dcd7714a9007accff4aad58ff6701a5896b1
 const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
